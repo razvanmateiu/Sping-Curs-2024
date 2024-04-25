d645014bd5c71ca4134c70e79a2dcc5e64526f98
Razvan <razvan.mateiu@gmail.com>
2024-04-25 18:32:38 +0200

    Upload FirstApp

---
+++ FirstApp/src/main/java/com/example/FirstApp/FirstAppNameApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppNameApplication.class, args);
+	}
+
+}
